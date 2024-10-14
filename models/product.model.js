c0002a1bd0a64332f52583735a6ad8d5d58d604e
Salma Ghrabi <salmaghrabi6@gmail.com>
2024-10-15 00:09:12 +0100

    first_commit

---
+++ models/product.model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter product name"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "Please enter product quantity"],
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: 0,
+      default: 0,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+module.exports = Product;
